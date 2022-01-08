b857093ec213f07b069dc89de9513e61cbf1d7d2
Pitako <robertosavio@gmail.com>
2022-01-08 14:42:53 -0300

    Criação classe de manipulação do CSV

---
+++ atualizarCSV.py
@@ -0,0 +1,18 @@
+class atualizarCSV:
+
+    def __init__(self) -> None:
+        pass
+
+
+    def criarArquivo():
+        pass
+
+    def carregarLinhasArquivo(self):
+        pass
+
+    def apagarLinha(self, titulo):
+        pass
+
+    def inserirLinha(self, texto):
+        pass
+
